0f5fa5f77b1c08e283ca4033e6262f957acc8cba
Marcin <marcin.szturminski@gmail.com>
2024-12-11 18:59:20 +0100

    added first version of mobile nav - still some shadow needed on body

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,31 @@
-const cookieBox = document.querySelector('.cookie__box')
-const cookieBtn = document.querySelector('.cookie__btn')
+const hamburgerBtn = document.querySelector('.hamburger')
+const navMobileIcon = document.querySelector('.header__icon')
+const navMobileTitle = document.querySelector('.header__title')
+const navMobile = document.querySelector('.nav__mobile')
 
-const showCookie = () => {
-	const cookieEten = localStorage.getItem('cookie')
-	if (cookieEten) {
-		cookieBox.classList.add('hide')
-	}
+const showMobileNav = () => {
+	hamburgerBtn.classList.toggle('is-active')
+	navMobileIcon.classList.toggle('show-nav-header')
+	navMobileTitle.classList.toggle('show-nav-header')
+	navMobile.classList.toggle('show-mobile-nav')
 }
 
-const handleCookieBox = () => {
-	localStorage.setItem('cookie', 'true')
-	cookieBox.classList.add('hide')
-}
+hamburgerBtn.addEventListener('click', showMobileNav)
 
-cookieBtn.addEventListener('click', handleCookieBox)
-showCookie()
+// const cookieBox = document.querySelector('.cookie__box')
+// const cookieBtn = document.querySelector('.cookie__btn')
+
+// const showCookie = () => {
+// 	const cookieEten = localStorage.getItem('cookie')
+// 	if (cookieEten) {
+// 		cookieBox.classList.add('hide')
+// 	}
+// }
+
+// const handleCookieBox = () => {
+// 	localStorage.setItem('cookie', 'true')
+// 	cookieBox.classList.add('hide')
+// }
+
+// cookieBtn.addEventListener('click', handleCookieBox)
+// showCookie()
